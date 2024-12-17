7859871b072d219d4711e6a9153b4f2dbc288a98
qbert0 <126874637+qbert0@users.noreply.github.com>
2024-12-17 17:55:53 +0700

    fix final

--- components/Card/CarouselDot/tsx/EmblaCarouselItem.tsx
+++ components/Card/CarouselDot/tsx/EmblaCarouselItem.tsx
@@ -11,11 +11,11 @@ const EmblaCarouselItem = ({data} : Props) => {
 
     return (
         <div className="w-full bg-black">
-            <Link href={`/movie/${data.id}`} className="w-full">
+            <div className="w-full">
                 <div className=" block w-full m-auto">
                     <img src={`https://image.tmdb.org/t/p/original${data && data.image}`} alt={data.image} className=" object-cover w-full"/>
                 </div>
-                <div className=" absolute bottom-32 left-32 w-1/2 space-y-10">
+                < Link href={`/movie/${data.id}`} className=" absolute bottom-32 left-32 w-1/2 space-y-10">
                     <div className="text-7xl text-white font-bold mb-0">
                         {data.name}
                     </div>
@@ -32,10 +32,8 @@ const EmblaCarouselItem = ({data} : Props) => {
                     <div className="text-2xl italic text-white ">
                         {data.overView}
                     </div>
-                    
-
-                </div>
-        </Link>
+                </ Link>
+        </div>
         </div>
     )
 }
--- components/Shared/header/avatar.tsx
+++ components/Shared/header/avatar.tsx
@@ -59,6 +59,5 @@ const Avatars = () => {
     )
 }
 
-
 export default Avatars
 
